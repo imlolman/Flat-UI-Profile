b300bb533f7830065a32fff85d0078d4f70d31c5
imlolman <satyam.gupta756@gmail.com>
2018-03-20 18:20:35 +0530

    Added Call Permissions and Now Both the Buttons Are Working. 1.) Redirect to My Link 2.) Makes Call

--- app/src/main/java/com/satyam/lolman/profile/MainActivity.java
+++ app/src/main/java/com/satyam/lolman/profile/MainActivity.java
@@ -1,9 +1,19 @@
 package com.satyam.lolman.profile;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int REQUEST_PERMISSION_PHONE_STATE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void call(View view) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},REQUEST_PERMISSION_PHONE_STATE);
+
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse(getResources().getString(R.string.mobile)));
+    }
+
+    public void redirect(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.redirect_link)));
+        startActivity(browserIntent);
+    }
 
 }
